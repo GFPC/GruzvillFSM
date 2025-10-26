fd0dbe12563d9d79d87515c1950316e52b81366f
GFP <fealx15@gmail.com>
2025-10-26 20:58:24 +0700

    a

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,5 @@
 const API_BASE = "http://188.225.44.153:3000/api";
+
 export const api = {
     getAvailableActions: (entity_type: string, entity_id: number, role: string) =>
         fetch(`${API_BASE}/ui-actions?entity_type=${entity_type}&entity_id=${entity_id}&role=${role}`).then(r => r.json()),
